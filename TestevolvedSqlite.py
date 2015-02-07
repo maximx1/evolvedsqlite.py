91e5617f848bcaeaa6604dd284112eb82d39b77b
Justin Walrath <walrathjaw@gmail.com>
2015-02-06 22:54:53 -0600

    Updated some tests
    Need to verify that the tests actually run...

--- TestevolvedSqlite.py
+++ TestevolvedSqlite.py
@@ -14,6 +14,17 @@ class TestEvolvedSqlite(unittest.TestCase):
 	def test_filterInstalledVersionCanFilterMultipleVersions():
 		self.assertFalse(filterInstalledVersion(self.files, 2))
 	
+	def test_canSortMultipleSqlFiles():
+		unsortedFiles = ["4.sql", "2.sql", "1.sql", "4.sql"]
+		self.assertEqual(sortNumberedSqlFiles(unsortedFiles), self.files)
+		
+	def test_canSortOneSqlFile():
+		oneFile = ["1.sql"]
+		self.assertEqual(["1.sql"], sortNumberedSqlFiles(oneFile))
+		
+	def test_canSortEmptyList():
+		noneFile = []
+		self.assertEmpty(sortNumberedSqlFiles(noneFile))
 		
 if __name__ == '__main__':
 	unittest.main()
