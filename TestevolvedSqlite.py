bbf26898586326db2062e1c0a47778cc5dfcd586
justin <walrathjaw@gmail.com>
2015-02-10 17:45:29 -0600

    Fixed broken tests.

--- TestevolvedSqlite.py
+++ TestevolvedSqlite.py
@@ -2,29 +2,29 @@ import unittest
 from evolvedsqlite import *
 
 class TestEvolvedSqlite(unittest.TestCase):
-	def setUp():
+	def setUp(self):
 		self.files = ["1.sql", "2.sql", "3.sql", "4.sql"]
 
-	def test_filterInstalledVersionCanFilterAllVersions():
-		self.assertFalse(filterInstalledVersion(self.files, 4))
+	def test_filterInstalledVersionCanFilterAllVersions(self):
+		self.assertListEqual(filterInstalledVersion(self.files, 4), [])
 
-	def test_filterInstalledVersionCanFilterOnlyOneVersion():
-		self.assertFalse(filterInstalledVersion(self.files, 1))
+	def test_filterInstalledVersionCanFilterOnlyOneVersion(self):
+		self.assertListEqual(filterInstalledVersion(self.files, 1), ["2.sql", "3.sql", "4.sql"])
 
-	def test_filterInstalledVersionCanFilterMultipleVersions():
-		self.assertFalse(filterInstalledVersion(self.files, 2))
+	def test_filterInstalledVersionCanFilterMultipleVersions(self):
+		self.assertListEqual(filterInstalledVersion(self.files, 2), ["3.sql", "4.sql"])
 	
-	def test_canSortMultipleSqlFiles():
-		unsortedFiles = ["4.sql", "2.sql", "1.sql", "4.sql"]
-		self.assertEqual(sortNumberedSqlFiles(unsortedFiles), self.files)
+	def test_canSortMultipleSqlFiles(self):
+		unsortedFiles = ["4.sql", "2.sql", "1.sql", "3.sql"]
+		self.assertListEqual(sortNumberedSqlFiles(unsortedFiles), self.files)
 		
-	def test_canSortOneSqlFile():
+	def test_canSortOneSqlFile(self):
 		oneFile = ["1.sql"]
-		self.assertEqual(["1.sql"], sortNumberedSqlFiles(oneFile))
+		self.assertListEqual(["1.sql"], sortNumberedSqlFiles(oneFile))
 		
-	def test_canSortEmptyList():
+	def test_canSortEmptyList(self):
 		noneFile = []
-		self.assertEmpty(sortNumberedSqlFiles(noneFile))
+		self.assertListEqual(sortNumberedSqlFiles(noneFile), [])
 		
 if __name__ == '__main__':
 	unittest.main()
